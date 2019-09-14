c957de702d51fb1ee005eaa48c8cfbe106034103
Florian Ntibarigobeka <44535627+floriann2@users.noreply.github.com>
2019-09-14 03:46:46 -0400

    Update entraction.php

--- entraction.php
+++ entraction.php
@@ -1,13 +1,14 @@
 
 	<?php
-			
+		include('db_connect.php');
+	
 		$conn = mysqli_connect("localhost", "florian", "61honest51", "entr_testdb");
 			
 		if (!$conn){
 			echo 'Connection error: ' . mysqli_connect_error();
 		}
 		// write query for all entries
-			$sql = 'SELECT *FROM bookings';
+			$sql = 'SELECT * FROM bookings';
 
 				
 		// get the result set (set of rows)
@@ -18,58 +19,31 @@
 		// free the $result from memory (good practise)
 		    mysqli_free_result($result);
 							
-		// close connection
-			mysqli_close($conn);
 				
 		//explode(',', $entries[0]['name']);
-
-	include('db_connect.php');
 
 	$name = $company = $venue = '';
 	$errors = array('name' => '', 'company' => '', 'venue' => '');
 
 	if(isset($_POST['submit'])){
-		
 		// check name
-		if(empty($_POST['name'])){
-			$errors['name'] = 'An name is required';
-		} else{
-			$name = $_POST['name'];
-			if(!filter_var($name, FILTER_VALIDATE_EMAIL)){
-				$errors['name'] = 'Email must be a valid name address';
-			}
-		}
+		$name = $_POST['name'];
 
 		// check company
-		if(empty($_POST['company'])){
-			$errors['company'] = 'A company is required';
-		} else{
-			$company = $_POST['company'];
-			if(!preg_match('/^[a-zA-Z\s]+$/', $company)){
-				$errors['company'] = 'Title must be letters and spaces only';
-			}
-		}
+		$company = $_POST['company'];
 
 		// check venue
-		if(empty($_POST['venue'])){
-			$errors['venue'] = 'At least one ingredient is required';
-		} else{
-			$venue = $_POST['venue'];
-			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $venue)){
-				$errors['venue'] = 'Ingredients must be a comma separated list';
-			}
-		}
+		$venue = $_POST['venue'];
 
 		if(array_filter($errors)){
-			//echo 'errors in form';
+			//display 'errors in form';
 		} else {
 			// escape sql chars
 			$name = mysqli_real_escape_string($conn, $_POST['name']);
 			$company = mysqli_real_escape_string($conn, $_POST['company']);
 			$venue = mysqli_real_escape_string($conn, $_POST['venue']);
-
 			// create sql
-			$sql = "INSERT INTO bookings(company,name,venue) VALUES('$company','$name','$venue')";
+			$sql = "INSERT INTO bookings(name,company,venue) VALUES('$name','$company','$venue')";
 
 			// save to db and check
 			if(mysqli_query($conn, $sql)){
@@ -82,6 +56,8 @@
 		}
 
 	} // end POST check
+	// close connection
+	mysqli_close($conn);
 
 ?>
 <!DOCTYPE HTML>
