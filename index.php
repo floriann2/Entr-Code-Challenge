917ff3c5a712aba91cad4d68f15af8a7d0fc05a7
Florian Ntibarigobeka <44535627+floriann2@users.noreply.github.com>
2019-09-14 03:46:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,21 +11,21 @@
 			<h1>Welcome!</h1>
 			<h3>Please enter your details below, to be assigned to a group: </h3>
 			<!--<p><span class="error">* required field</span></p>-->
-			<form action= "entraction.php" method="post">
+			<form action= "entraction.php" method="POST">
 				<fieldset>
 					<legend>User Information</legend>
 					<p>Name: <!--<span class="error">*--></p><input type="text" name="name"></span>
 					<p>Company:</p><input type="text" name="company">
 					<p>Which curated space are you most interested in renting? <!--<span class="error">*--></p></span>
-					<input type="radio" name="venue" value="hotel">Hotel
-					<input type="radio" name="venue" value="bar">Bar
-					<input type="radio" name="venue" value="cafe">Cafe
-					<input type="radio" name="venue" value="studio">Studio
-					<input type="radio" name="venue" value="park">Park
-					<input type="radio" name="venue" value="other">Other
-					<p>If you've selected 'Other', please indicate the venue type below:</p>
-					<input type="text" name="venue"><br><br><br>
-				<button id="submit">Submit</button>
+					<input type="radio" name="venue" value="Hotel">Hotel
+					<input type="radio" name="venue" value="Bar">Bar
+					<input type="radio" name="venue" value="Cafe">Cafe
+					<input type="radio" name="venue" value="Studio">Studio
+					<input type="radio" name="venue" value="Park">Park
+					<input type="radio" name="venue" value="Other">Other
+					<!--<p>If you've selected 'Other', please indicate the venue type below:</p>
+					<input type="text" name="venue1">--><br><br><br>
+					<input type="submit" name="submit" value="Submit">
 				</fieldset>
 			</form>
 		</body>
