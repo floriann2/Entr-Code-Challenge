ab676d75707fe985d759b36393b64793445f58dd
Florian Ntibarigobeka <44535627+floriann2@users.noreply.github.com>
2019-09-16 14:55:15 -0400

    Update entraction.php

--- entraction.php
+++ entraction.php
@@ -2,7 +2,7 @@
 	<?php
 		include('db_connect.php');
 	
-		$conn = mysqli_connect("localhost", "florian", "61honest51", "entr_testdb");
+		$conn = mysqli_connect("", "", "", "entr_testdb");
 			
 		if (!$conn){
 			echo 'Connection error: ' . mysqli_connect_error();
